b9ae7b8c396bf5cd93379392f4eff01c2e6b55fd
saifulislam0784 <saifulislam0784@gmail.com>
2021-03-22 11:48:35 +0600

    Add registration feature into admin template

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,4 @@ Route::get('admin/dashboard', [App\Http\Controllers\AdminController::class , 'Sh
 
 Route::post('admin/login', [App\Http\Controllers\Auth\LoginController::class , 'login']) ->name('admin.login');
 Route::post('admin/logout', [App\Http\Controllers\Auth\LoginController::class , 'logout']) ->name('admin.logout');
+Route::post('admin/register', [App\Http\Controllers\Auth\RegisterController::class , 'register']) ->name('admin.register');
