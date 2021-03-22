5009016351a610f75772db61ca35890dc462b3a3
saifulislam0784 <saifulislam0784@gmail.com>
2021-03-22 19:12:06 +0600

    Add admin template slicing

---
+++ resources/views/admin/layouts/sidemenu.blade.php
@@ -0,0 +1,70 @@
+<!-- Sidebar -->
+<div class="sidebar" id="sidebar">
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu">
+            <ul>
+                <li class="menu-title">
+                    <span>Main</span>
+                </li>
+                <li class="active">
+                    <a href="{{route('admin.dashboard')}}"><i class="fe fe-home"></i> <span>Dashboard</span></a>
+                </li>
+
+
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-document"></i> <span> Blog </span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="login.html"> Posts </a></li>
+                        <li><a href="register.html"> catagory </a></li>
+                        <li><a href="forgot-password.html"> Tag </a></li>
+
+                    </ul>
+                </li>
+
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-document"></i> <span> Pages </span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="login.html"> Pages </a></li>
+                        <li><a href="register.html"> catagory </a></li>
+                        <li><a href="forgot-password.html"> Tag </a></li>
+
+                    </ul>
+                </li>
+
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-document"></i> <span> Products </span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="login.html"> Products </a></li>
+                        <li><a href="register.html"> catagory </a></li>
+                        <li><a href="forgot-password.html"> Tag </a></li>
+                        <li><a href="forgot-password.html"> Brand </a></li>
+
+                    </ul>
+                </li>
+
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-document"></i> <span> Orders </span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="login.html"> Orders </a></li>
+                        <li><a href="register.html"> Reports </a></li>
+                    </ul>
+                </li>
+
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-document"></i> <span> Users </span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="login.html"> Users </a></li>
+                        <li><a href="register.html"> Role </a></li>
+                        <li><a href="register.html"> Permission </a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="profile.html"><i class="fe fe-user-plus"></i> <span>Settings</span></a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- /Sidebar -->
